9eb0c65b78e72e94d0e3d90abb9ab5f100302606
texas2010 <9701609+texas2010@users.noreply.github.com>
2021-06-10 11:41:00 -0500

    add DOM event listen

--- src/ContentScript/index.ts
+++ src/ContentScript/index.ts
@@ -1,3 +1,16 @@
-console.log('hello world from content script');
+import tempMessage from './temp-message';
+import listenDOM from './listen-dom';
+
+window.addEventListener('load', () => {
+  console.log('window loaded');
+
+  tempMessage();
+  listenDOM();
+
+  if (document.location.pathname === '/browse/my-list') {
+    // Install feature in the my list page.
+    console.log('Installed feature in the my list page');
+  }
+});
 
 export {};
---
+++ src/ContentScript/listen-dom.ts
@@ -0,0 +1,29 @@
+const observerOptions = {
+  attributes: true,
+  characterData: true,
+  childList: true,
+  subtree: true,
+  attributeOldValue: true,
+  characterDataOldValue: true,
+};
+
+const targetAppMountPointElement: Element | null =
+  document.querySelector('#appMountPoint');
+
+let oldHref = document.location.href;
+
+const mutationObserver = new MutationObserver((mutations) => {
+  mutations.forEach(() => {
+    if (oldHref !== document.location.href) {
+      oldHref = document.location.href;
+      console.log(document.location.href);
+    }
+  });
+});
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default (): void => {
+  if (targetAppMountPointElement) {
+    mutationObserver.observe(targetAppMountPointElement, observerOptions);
+  }
+};
