fdd9a55a35fedf6eb67e4c4c66d9e30018d83b4d
texas2010 <9701609+texas2010@users.noreply.github.com>
2021-06-20 14:51:20 -0500

    fix format issue and clean up in the pop up files

--- src/Background/index.ts
+++ src/Background/index.ts
@@ -1,5 +1,6 @@
 import 'emoji-log';
-import {browser} from 'webextension-polyfill-ts';
+// eslint-disable-next-line prettier/prettier
+import { browser } from 'webextension-polyfill-ts';
 
 browser.runtime.onInstalled.addListener((): void => {
   console.emoji('🦄', 'extension installed');
--- src/Popup/Popup.tsx
+++ src/Popup/Popup.tsx
@@ -1,16 +1,18 @@
-import * as React from 'react';
-import {browser, Tabs} from 'webextension-polyfill-ts';
+import React from 'react';
+// eslint-disable-next-line prettier/prettier
+import { browser, Tabs } from 'webextension-polyfill-ts';
 
 import './styles.scss';
 
 function openWebPage(url: string): Promise<Tabs.Tab> {
-  return browser.tabs.create({url});
+  // eslint-disable-next-line prettier/prettier
+  return browser.tabs.create({ url });
 }
 
 const Popup: React.FC = () => {
   return (
     <section id="popup">
-      <h2>WEB-EXTENSION-STARTER</h2>
+      <h2>Hello Netflix</h2>
       <button
         id="options__button"
         type="button"
@@ -20,34 +22,6 @@ const Popup: React.FC = () => {
       >
         Options Page
       </button>
-      <div className="links__holder">
-        <ul>
-          <li>
-            <button
-              type="button"
-              onClick={(): Promise<Tabs.Tab> => {
-                return openWebPage(
-                  'https://github.com/abhijithvijayan/web-extension-starter'
-                );
-              }}
-            >
-              GitHub
-            </button>
-          </li>
-          <li>
-            <button
-              type="button"
-              onClick={(): Promise<Tabs.Tab> => {
-                return openWebPage(
-                  'https://www.buymeacoffee.com/abhijithvijayan'
-                );
-              }}
-            >
-              Buy Me A Coffee
-            </button>
-          </li>
-        </ul>
-      </div>
     </section>
   );
 };
