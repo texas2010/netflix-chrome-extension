d3c5d6735e56f5c4a9b0f35aa2049e24bd321a9d
texas2010 <9701609+texas2010@users.noreply.github.com>
2022-01-24 20:40:56 -0600

    clean up and redo again.

--- src/Background/index.ts
+++
@@ -1,7 +0,0 @@
-import 'emoji-log';
-// eslint-disable-next-line prettier/prettier
-import { browser } from 'webextension-polyfill-ts';
-
-browser.runtime.onInstalled.addListener((): void => {
-  console.emoji('🦄', 'extension installed');
-});
