3ebc0dd3ac9bece3dfc8fa5be905843e74173ea8
texas2010 <9701609+texas2010@users.noreply.github.com>
2022-01-26 22:45:16 -0600

    fix some files

--- src/options/index.tsx
+++ src/options/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import Options from '../components/Options';
+import './index.css';
+document.title = 'Options Page';
 console.log('Options file');
 
 ReactDOM.render(<Options />, document.getElementById('root'));
--- src/popup/index.tsx
+++ src/popup/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 
 import Popup from '../components/Popup';
 import './index.css';
+document.title = 'Popup Page';
 console.log('popup file');
 
 ReactDOM.render(<Popup />, document.getElementById('root'));
