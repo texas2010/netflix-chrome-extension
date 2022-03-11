ece81ec8476bd393de58ad572c3386e94ee0344e
texas2010 <9701609+texas2010@users.noreply.github.com>
2022-03-10 22:09:18 -0600

    develop create manifest file script and unit test for that script.

---
+++ src/utils/createManifestJson/createManifest.test.ts
@@ -0,0 +1,315 @@
+import afs from 'fs/promises';
+import path from 'path';
+
+import createManifestFile, { error } from './createManifestFile';
+
+describe('createManifestFile function', () => {
+  test('should throw error when manifest.config.json is not exist', async () => {
+    const filename = `${path.resolve('./fake-test')}/manifest.config.json`;
+    const expected = error.fileRequired;
+
+    await expect(createManifestFile(filename)).rejects.toThrowError(expected);
+  });
+  test('should throw error when manifest.config.json is exist but empty', async () => {
+    const filename = `${path.resolve('./fake-test')}/manifest.config.json`;
+    const input = '';
+    const expected = error.fileExistEmpty;
+
+    await afs.writeFile(filename, input);
+
+    await expect(createManifestFile(filename)).rejects.toThrowError(expected);
+
+    await afs.unlink(filename);
+  });
+  test('should throw error when it is not object in the manifest.config.json', async () => {
+    const filename = `${path.resolve('./fake-test')}/manifest.config.json`;
+    const input: any[] = [];
+    const expected = error.objectRequired;
+
+    await afs.writeFile(filename, JSON.stringify(input));
+
+    await expect(createManifestFile(filename)).rejects.toThrowError(expected);
+
+    await afs.unlink(filename);
+  });
+  test('should throw error when object is empty in the manifest.config.json', async () => {
+    const filename = `${path.resolve('./fake-test')}/manifest.config.json`;
+    const input = {};
+    const expected = error.objectEmpty;
+
+    await afs.writeFile(filename, JSON.stringify(input));
+
+    await expect(createManifestFile(filename)).rejects.toThrowError(expected);
+
+    await afs.unlink(filename);
+  });
+  test('should throw error when object do not have correct property', async () => {
+    const filename = `${path.resolve('./fake-test')}/manifest.config.json`;
+    const input = { asdf: '', fdsa: 4, name: 'test' };
+    const expected = error.objectWrongProperty;
+
+    await afs.writeFile(filename, JSON.stringify(input));
+
+    await expect(createManifestFile(filename)).rejects.toThrowError(expected);
+
+    await afs.unlink(filename);
+  });
+  test('should throw error when name, description, and options_page is not string', async () => {
+    const filename = `${path.resolve('./fake-test')}/manifest.config.json`;
+    const input = {
+      name: 5,
+      description: [],
+      options_page: {},
+      manifest_version: 3,
+      permissions: [],
+      content_scripts: {},
+      action: {},
+      icons: {},
+      background: {},
+    };
+    const expected = /must be string/;
+    await afs.writeFile(filename, JSON.stringify(input));
+    await expect(createManifestFile(filename)).rejects.toThrowError(expected);
+    await afs.unlink(filename);
+  });
+  test('should throw error when name, description, and options_page string is empty', async () => {
+    const filename = `${path.resolve('./fake-test')}/manifest.config.json`;
+    const input = {
+      name: '',
+      description: '',
+      options_page: '',
+      manifest_version: 3,
+      permissions: [],
+      content_scripts: [],
+      action: {},
+      icons: {},
+      background: {},
+    };
+    const expected = /can't be empty/;
+    await afs.writeFile(filename, JSON.stringify(input));
+    await expect(createManifestFile(filename)).rejects.toThrowError(expected);
+    await afs.unlink(filename);
+  });
+  test('should throw error when manifest_version is not number', async () => {
+    const filename = `${path.resolve('./fake-test')}/manifest.config.json`;
+    const input = {
+      name: 'title',
+      description: 'description',
+      options_page: 'options.html',
+      manifest_version: {},
+      permissions: [],
+      content_scripts: {},
+      action: {},
+      icons: {},
+      background: {},
+    };
+    const expected = /must be number./;
+    await afs.writeFile(filename, JSON.stringify(input));
+    await expect(createManifestFile(filename)).rejects.toThrowError(expected);
+    await afs.unlink(filename);
+  });
+  test('should throw error when permissions and content_scripts is not array', async () => {
+    const filename = `${path.resolve('./fake-test')}/manifest.config.json`;
+    const input = {
+      name: 'title',
+      description: 'description',
+      options_page: 'options.html',
+      manifest_version: 3,
+      permissions: {},
+      content_scripts: {},
+      action: {},
+      icons: {},
+      background: {},
+    };
+    const expected = /must be array/;
+    await afs.writeFile(filename, JSON.stringify(input));
+    await expect(createManifestFile(filename)).rejects.toThrowError(expected);
+    await afs.unlink(filename);
+  });
+  test(`should throw error when content_scripts' array is empty`, async () => {
+    const filename = `${path.resolve('./fake-test')}/manifest.config.json`;
+    const input = {
+      name: 'title',
+      description: 'description',
+      options_page: 'options.html',
+      manifest_version: 3,
+      permissions: [],
+      content_scripts: [],
+      action: {},
+      icons: {},
+      background: {},
+    };
+    const expected = /array can't be empty/;
+    await afs.writeFile(filename, JSON.stringify(input));
+    await expect(createManifestFile(filename)).rejects.toThrowError(expected);
+    await afs.unlink(filename);
+  });
+  test(`should throw error when content_scripts' array's element don't have object`, async () => {
+    const filename = `${path.resolve('./fake-test')}/manifest.config.json`;
+    const input = {
+      name: 'title',
+      description: 'description',
+      options_page: 'options.html',
+      manifest_version: 3,
+      permissions: [],
+      content_scripts: ['', 4],
+      action: {},
+      icons: {},
+      background: {},
+    };
+    const expected = /array must have object in the each element./;
+    await afs.writeFile(filename, JSON.stringify(input));
+    await expect(createManifestFile(filename)).rejects.toThrowError(expected);
+    await afs.unlink(filename);
+  });
+  test(`should throw error when content_scripts' array's each object's properties do not have matches, css and js`, async () => {
+    const filename = `${path.resolve('./fake-test')}/manifest.config.json`;
+    const input = {
+      name: 'title',
+      description: 'description',
+      options_page: 'options.html',
+      manifest_version: 3,
+      permissions: [],
+      content_scripts: [{}],
+      action: {},
+      icons: {},
+      background: {},
+    };
+    const expected = /each object's properties must be matches, css and js/;
+    await afs.writeFile(filename, JSON.stringify(input));
+    await expect(createManifestFile(filename)).rejects.toThrowError(expected);
+    await afs.unlink(filename);
+  });
+  test(`should throw error when content_scripts' array's each object's values is not array`, async () => {
+    const filename = `${path.resolve('./fake-test')}/manifest.config.json`;
+    const input = {
+      name: 'title',
+      description: 'description',
+      options_page: 'options.html',
+      manifest_version: 3,
+      permissions: [],
+      content_scripts: [
+        {
+          matches: '',
+          css: [],
+          js: [],
+        },
+      ],
+      action: {},
+      icons: {},
+      background: {},
+    };
+    const expected = /must be array/;
+    await afs.writeFile(filename, JSON.stringify(input));
+    await expect(createManifestFile(filename)).rejects.toThrowError(expected);
+    await afs.unlink(filename);
+  });
+  test(`should throw error when content_scripts' array's each object's values' element is empty`, async () => {
+    const filename = `${path.resolve('./fake-test')}/manifest.config.json`;
+    const input = {
+      name: 'title',
+      description: 'description',
+      options_page: 'options.html',
+      manifest_version: 3,
+      permissions: [],
+      content_scripts: [
+        {
+          matches: [],
+          css: [],
+          js: [],
+        },
+      ],
+      action: {},
+      icons: {},
+      background: {},
+    };
+    const expected = /can't be empty/;
+    await afs.writeFile(filename, JSON.stringify(input));
+    await expect(createManifestFile(filename)).rejects.toThrowError(expected);
+    await afs.unlink(filename);
+  });
+  test(`should throw error when content_scripts' array's each object's values' element is not string`, async () => {
+    const filename = `${path.resolve('./fake-test')}/manifest.config.json`;
+    const input = {
+      name: 'title',
+      description: 'description',
+      options_page: 'options.html',
+      manifest_version: 3,
+      permissions: [],
+      content_scripts: [
+        {
+          matches: [4],
+          css: [],
+          js: [],
+        },
+      ],
+      action: {},
+      icons: {},
+      background: {},
+    };
+    const expected = /array's element must be string/;
+    await afs.writeFile(filename, JSON.stringify(input));
+    await expect(createManifestFile(filename)).rejects.toThrowError(expected);
+    await afs.unlink(filename);
+  });
+  test(`should throw error when content_scripts' array's each object's values' element string can't be empty`, async () => {
+    const filename = `${path.resolve('./fake-test')}/manifest.config.json`;
+    const input = {
+      name: 'title',
+      description: 'description',
+      options_page: 'options.html',
+      manifest_version: 3,
+      permissions: [],
+      content_scripts: [
+        {
+          matches: ['asdf'],
+          css: [''],
+          js: [],
+        },
+      ],
+      action: {},
+      icons: {},
+      background: {},
+    };
+    const expected = /string can't be empty/;
+    await afs.writeFile(filename, JSON.stringify(input));
+    await expect(createManifestFile(filename)).rejects.toThrowError(expected);
+    await afs.unlink(filename);
+  });
+
+  test('should get resolve when information is correct', async () => {
+    const filename = `${path.resolve('./fake-test')}/manifest.config.json`;
+    const input = {
+      name: 'title',
+      description: 'just description',
+      options_page: 'asdf.html',
+      manifest_version: 3,
+      permissions: [],
+      content_scripts: [
+        {
+          matches: ['*://*.netflix.com/*'],
+          css: ['./static/css/contentScript.css'],
+          js: ['./static/js/contentScript.js'],
+        },
+      ],
+      action: {
+        default_popup: 'popup.html',
+        default_title: 'Open the popup',
+      },
+      icons: {
+        '16': 'assets/favicon-16.png',
+        '48': 'assets/favicon-48.png',
+        '128': 'assets/favicon-128.png',
+      },
+      background: {
+        service_worker: './static/js/background.js',
+      },
+    };
+
+    await afs.writeFile(filename, JSON.stringify(input));
+
+    await expect(createManifestFile(filename)).resolves.toBeTruthy();
+
+    await afs.unlink(filename);
+  });
+});
---
+++ src/utils/createManifestJson/createManifestFile.ts
@@ -0,0 +1,229 @@
+import afs from 'fs/promises';
+import path from 'path';
+// import getPackageInfo from './getPackageInfo';
+
+type contentScriptsArrPropType = {
+  matches: string[];
+  css: string[];
+  js: string[];
+};
+
+interface ManifestConfigI {
+  name: string;
+  description: string;
+  version: string;
+  manifest_version: number;
+  action: {
+    default_popup: string;
+    default_title: string;
+  };
+  content_scripts: contentScriptsArrPropType[];
+  permissions: string[];
+  icons: {
+    16: string;
+    48: string;
+    128: string;
+  };
+  options_page: string;
+  background: {
+    service_worker: string;
+  };
+}
+
+export const error = {
+  fileRequired:
+    'manifest.config.json is not exist. it is required to have a file',
+  fileExistEmpty:
+    'manifest.config.json file is empty. it must have information.',
+  objectEmpty:
+    'Object is empty. it must have properties. properties: name, description, manifest_version, action, content_scripts, permissions, icons, options_page, background!',
+  objectRequired: 'object is required in the manifest.config.json file',
+  objectWrongProperty:
+    'property need to be correct. properties: name, description, manifest_version, action, content_scripts, permissions, icons, options_page, background',
+};
+
+const allPropArr = [
+  'name',
+  'description',
+  'manifest_version',
+  'action',
+  'content_scripts',
+  'permissions',
+  'icons',
+  'options_page',
+  'background',
+];
+
+const createManifestFile = async (
+  filename: string = `${path.resolve('./')}/manifest.config.json`
+) => {
+  try {
+    //check if manifest.config.json is exist
+    await afs.access(filename);
+
+    // get data from manifest.config.json file
+    const dataStr = await afs.readFile(filename, 'utf8');
+
+    // check and throw error when it is empty string.
+    if (!dataStr) {
+      throw new Error(error.fileExistEmpty);
+    }
+
+    const dataObj: ManifestConfigI = JSON.parse(dataStr);
+
+    // check and throw error when it is not object.
+    if (Object.prototype.toString.call(dataObj) !== '[object Object]') {
+      throw new Error(error.objectRequired);
+    }
+
+    // check and throw error when object is empty
+    if (Object.keys(dataObj).length === 0) {
+      throw new Error(error.objectEmpty);
+    }
+
+    // check and throw error when it is not exist.
+    // name, description, manifest_version, action, content_scripts, permissions, icons, options_page, background
+    if (!allPropArr.every((prop) => prop in dataObj)) {
+      throw new Error(error.objectWrongProperty);
+    }
+
+    // check manifest config file
+    for (const dataKey in dataObj) {
+      switch (dataKey) {
+        case 'name':
+        case 'description':
+        case 'options_page':
+          // check and make sure it is string.
+          if (typeof dataObj[dataKey] !== 'string') {
+            throw new Error(`${dataKey} must be string.`);
+          }
+
+          // check and make sure it is not empty
+          if (!dataObj[dataKey]) {
+            throw new Error(`${dataKey} can't be empty.`);
+          }
+
+          break;
+        case 'manifest_version':
+          // check and make sure it is number.
+          if (typeof dataObj[dataKey] !== 'number') {
+            throw new Error(`${dataKey} must be number.`);
+          }
+
+          break;
+        case 'permissions':
+        case 'content_scripts':
+          // check and make sure it is array
+          if (
+            Object.prototype.toString.call(dataObj[dataKey]) !==
+            '[object Array]'
+          ) {
+            throw new Error(`${dataKey} must be array`);
+          }
+
+          // check content_scripts data
+          if (dataKey === 'content_scripts') {
+            const contentScriptsArr = dataObj['content_scripts'];
+
+            // check and make sure something is exist in the array! if zero then it will throw the error.
+            if (!contentScriptsArr.length) {
+              throw new Error(`${dataKey}' array can't be empty.`);
+            }
+
+            // check each element in the content scripts array
+            for (const contentScriptsArrObj of contentScriptsArr) {
+              // check and make sure it is object.
+              if (
+                Object.prototype.toString.call(contentScriptsArrObj) !==
+                '[object Object]'
+              ) {
+                throw new Error(
+                  `content_scripts' array must have object in the each element.`
+                );
+              }
+
+              // check object's properties. it must have matches, css and js properties
+              if (
+                !['matches', 'css', 'js'].every(
+                  (prop) => prop in contentScriptsArrObj
+                )
+              ) {
+                throw new Error(
+                  `content_scripts' each object's properties must be matches, css and js`
+                );
+              }
+
+              // check each property in the contentScriptsArrObj.
+              for (const contentScriptsArrObjKey in contentScriptsArrObj) {
+                const contentScriptsArrObjArr =
+                  contentScriptsArrObj[
+                    contentScriptsArrObjKey as keyof contentScriptsArrPropType
+                  ];
+
+                // check contentScriptsArrObjArr and make sure it is only array.
+                if (
+                  Object.prototype.toString.call(contentScriptsArrObjArr) !==
+                  '[object Array]'
+                ) {
+                  throw new Error(`${contentScriptsArrObjKey} must be array`);
+                }
+
+                // check and make sure something is exist in the array. if zero then throw error.
+                if (!contentScriptsArrObjArr.length) {
+                  throw new Error(
+                    `${contentScriptsArrObjKey}'s array can't be empty.`
+                  );
+                }
+
+                // check each element in the contentScriptsArrObjArr
+                for (const contentScriptsArrObjArrElement of contentScriptsArrObjArr) {
+                  // check element in the array and make sure it is only string.
+                  if (typeof contentScriptsArrObjArrElement !== 'string') {
+                    throw new Error(
+                      `${contentScriptsArrObjKey}'s array's element must be string.`
+                    );
+                  }
+
+                  // check element and make sure string is not empty at all.
+                  if (!contentScriptsArrObjArrElement) {
+                    throw new Error(
+                      `${contentScriptsArrObjKey}'s array's element string can't be empty.`
+                    );
+                  }
+                }
+
+                // end loop contentScriptsArrObj
+              }
+
+              // end loop contentScriptsArr
+            }
+          }
+          break;
+        case 'action':
+        case 'icons':
+        case 'background':
+          break;
+        default:
+          throw new Error(`${dataKey} is not suppose to be in the object!`);
+      }
+    }
+
+    // const packageInfo = await getPackageInfo();
+    // if (!packageInfo) return;
+    // if (!packageInfo.version) {
+    //   throw new Error('in the Package.json, version is missing!');
+    // }
+    // const newManifestObj = { ...manifestObj, version: packageInfo.version };
+    // await afs.writeFile(`${path.resolve('./')}/build/manifest.json`, JSON.stringify(newManifestObj));
+    return true;
+  } catch (err: any) {
+    if (err.code === 'ENOENT') {
+      // package.json file is not exist!
+      throw new Error(error.fileRequired);
+    }
+
+    throw err;
+  }
+};
+
+export default createManifestFile;
