0c1005c23bcff12b2a8d206a06cca86b94f6c382
texas2010 <9701609+texas2010@users.noreply.github.com>
2022-03-17 22:15:02 -0500

    Update getPackageInfo.ts

--- src/utils/createManifestJson/getPackageInfo.ts
+++ src/utils/createManifestJson/getPackageInfo.ts
@@ -16,14 +16,14 @@ const getPackageInfo = async () => {
   }
 
   // check if build_path is not exist during test env
-  if (process.env.REAL_NODE_ENV === 'test' && !process.env.BUILD_PATH) {
+  if (process.env.TEST_NODE_ENV === 'test' && !process.env.BUILD_PATH) {
     throw new Error(
       'BUILD_PATH is not exist in the test env file. it is required to have'
     );
   }
 
   const buildPath =
-    process.env.REAL_NODE_ENV === 'test'
+    process.env.TEST_NODE_ENV === 'test'
       ? (process.env.BUILD_PATH as string)
       : './';
   const filename = `${path.resolve(buildPath)}/package.json`;
