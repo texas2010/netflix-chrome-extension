cfcf166ea2137933d7e6c281c56609311670a7c5
texas2010 <9701609+texas2010@users.noreply.github.com>
2022-03-18 10:36:07 -0500

    Update index.tsx

--- src/options/index.tsx
+++ src/options/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import OptionsPage from '../components/OptionsPage';
+
 document.title = 'Options Page';
 console.log('Options file');
 
