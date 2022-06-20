cc57c026eac6a81d6baf41f6d76042f1d89709da
texas2010 <9701609+texas2010@users.noreply.github.com>
2022-06-19 21:13:03 -0500

    clean up in the content script index file.

--- src/content-scripts/index.ts
+++ src/content-scripts/index.ts
@@ -1,27 +1,30 @@
 import { devLog } from '@services';
-import { isElementExist } from '@content-scripts/services';
 
-import { devBannerMessage } from './devBannerMessage';
-import { appRender } from './App';
+// import { isElementExist } from '@content-scripts/services';
+
+// import { devBannerMessage } from './devBannerMessage';
+// import { appRender } from './App';
 
 devLog('Content Script file');
 
 window.addEventListener('load', async () => {
   devLog('window loaded');
 
-  if (process.env.NODE_ENV === 'development') {
-    devBannerMessage();
-  }
+  devLog(new Date());
 
-  const mainAppRoot = document.createElement('div');
-  mainAppRoot.setAttribute('id', 'nAppRoot');
+  // if (process.env.NODE_ENV === 'development') {
+  //   devBannerMessage();
+  // }
 
-  const theirAppRoot: Element | null = document.getElementById('appMountPoint');
-  if ((await isElementExist('#appMountPoint')) && theirAppRoot) {
-    theirAppRoot.appendChild(mainAppRoot);
-    devLog('nAppRoot added in the dom!');
-    appRender('nAppRoot');
-  }
+  // const mainAppRoot = document.createElement('div');
+  // mainAppRoot.setAttribute('id', 'nAppRoot');
+
+  // const theirAppRoot: Element | null = document.getElementById('appMountPoint');
+  // if ((await isElementExist('#appMountPoint')) && theirAppRoot) {
+  //   theirAppRoot.appendChild(mainAppRoot);
+  //   devLog('nAppRoot added in the dom!');
+  //   appRender('nAppRoot');
+  // }
 
   devLog('end of window loading.');
 });
